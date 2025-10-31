7cc34297ba3826beaa9253c7c748cef44266b3fb
EkaitzGarcia <clase.ekaitz@gmail.com>
2025-10-31 11:37:57 +0100

    A

--- src/models/Persona.java
+++ src/models/Persona.java
@@ -1,31 +1,52 @@
 package models;
 
-public class Persona {
+public class Almacen {
 	
-	private String nombre;
-    private int edad;
-    private String dni;
-    
-    public Persona(String nombre, int edad, String dni) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = dni;
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
-    
-    public int getEdad() {
-        return edad;
-    }
-    
-    public String getDni() {
-        return dni;
-    }
-    
-    @Override
-    public String toString() {
-        return nombre + ", " + edad + " años, DNI " + dni;
-    }
+	private String articulo;
+	private int unidad;
+	boolean estado = true;
+	
+	//Getters
+	public String getArticulo() {
+		return articulo;
+	}
+	
+	public int getUnidades() {
+		return unidad;
+	}
+	
+	public boolean isEstado() {
+		return estado;
+	}
+	
+	//Setters
+	public void setArticulo(String articulo) {
+		this.articulo = articulo;
+	}
+	
+	public void setUnidades(int unidad) {
+		this.unidad = unidad;
+	}
+	
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	public Almacen(String articulo, int unidades, boolean estado) {
+		this.articulo = articulo;
+		this.unidad = unidades;
+		this.estado = estado;
+	}
+	
+
+	@Override
+	public String toString() {
+		if(estado == false) {
+		return "Almacen [Articulo = " + articulo + ", Unidades = " + unidad + ", Estado: Inactivo";
+		} else {
+			return "Almacen [Articulo = " + articulo + ", Unidades = " + unidad + ", Estado: Activo";
+		}
+	}
+	
+	
 }
